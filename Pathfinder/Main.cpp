b2f5574b24567b16580e4bcf487286f711a61ee0
Liljan <marcus.t.lilja@gmail.com>
2020-04-19 18:32:22 +0200

    SFML start-up project.

---
+++ Pathfinder/Main.cpp
@@ -0,0 +1,33 @@
+#include "SFML/Graphics.hpp"
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(1280, 720), "My window");
+	window.setFramerateLimit(60);
+
+	// run the program as long as the window is open
+	while(window.isOpen())
+	{
+		// check all the window's events that were triggered since the last iteration of the loop
+		sf::Event event;
+		while(window.pollEvent(event))
+		{
+			// "close requested" event: we close the window
+			if(event.type == sf::Event::Closed)
+				window.close();
+		}
+
+
+		// Rendering
+				// clear the window with black color
+		window.clear(sf::Color::Black);
+
+		// draw everything here...
+		// window.draw(...);
+
+		// end the current frame
+		window.display();
+	}
+
+	return 0;
+}
