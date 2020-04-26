df5d30c920ebfe530faaec882aa778aef7189c87
Liljan <marcus.t.lilja@gmail.com>
2020-04-26 22:25:56 +0200

    John Travolta

--- Pathfinder/Main.cpp
+++ Pathfinder/Main.cpp
@@ -2,6 +2,22 @@
 
 #include "PathFinder.h"
 
+void DrawNode(sf::RenderWindow& window, const Node& node, const sf::Vector2f& size, const sf::Color& color)
+{
+	const float x = node.coordinates.x * size.x;
+	const float y = node.coordinates.y * size.y;
+
+	sf::RectangleShape rect(sf::Vector2f(size.x - 1.0f, size.y - 1.0f));
+	rect.setPosition(sf::Vector2f(x, y));
+
+	rect.setFillColor(color);
+	rect.setOutlineColor(sf::Color(50, 50, 50, 255));
+
+	rect.setOutlineThickness(2.0f);
+
+	window.draw(rect);
+}
+
 void DrawPathFinder(sf::RenderWindow& window, PathFinder& pathFinder)
 {
 	const sf::Vector2u size = window.getSize();
@@ -9,93 +25,40 @@ void DrawPathFinder(sf::RenderWindow& window, PathFinder& pathFinder)
 	const float width = static_cast<float>(window.getSize().x / pathFinder.m_Width);
 	const float height = static_cast<float>(window.getSize().y / pathFinder.m_Height);
 
+	const sf::Vector2f dimensions(width, height);
+
 	for(const auto& node : pathFinder.m_Grid)
 	{
-		const float x = node.coordinates.x * width;
-		const float y = node.coordinates.y * height;
-
-		sf::RectangleShape rect(sf::Vector2f(width - 1.0f, height - 1.0f));
-		rect.setPosition(sf::Vector2f(x, y));
-
-		if(node.isWall)
-		{
-			rect.setFillColor(sf::Color(0, 0, 0, 255));
-			rect.setOutlineColor(sf::Color(50, 50, 50, 255));
-		}
-		else
-		{
-			rect.setFillColor(sf::Color(25, 25, 25, 255));
-			rect.setOutlineColor(sf::Color(50, 50, 50, 255));
-		}
-
-		rect.setOutlineThickness(2.0f);
-
-		window.draw(rect);
+		sf::Color color = node.isWall ? sf::Color(0, 0, 0, 255) : sf::Color(25, 25, 25, 255);
+		DrawNode(window, node, dimensions, color);
 	}
 
 	for(const auto& pNode : pathFinder.GetClosedNodes())
-	{
-		const float x = pNode->coordinates.x * width;
-		const float y = pNode->coordinates.y * height;
-
-		sf::RectangleShape rect(sf::Vector2f(width - 1.0f, height - 1.0f));
-		rect.setPosition(sf::Vector2f(x, y));
-
-		rect.setFillColor(sf::Color(255, 0, 0, 255));
-		rect.setOutlineColor(sf::Color(50, 50, 50, 255));
-
-		rect.setOutlineThickness(2.0f);
-
-		window.draw(rect);
-	}
+		DrawNode(window, *pNode, dimensions, sf::Color::Red);
 
 	for(const auto& pNode : pathFinder.GetOpenNodes())
+		DrawNode(window, *pNode, dimensions, sf::Color::Green);
+
+	if(const Node* pCurrentNode = pathFinder.GetCurrentNode())
 	{
-		const float x = pNode->coordinates.x * width;
-		const float y = pNode->coordinates.y * height;
-
-		sf::RectangleShape rect(sf::Vector2f(width - 1.0f, height - 1.0f));
-		rect.setPosition(sf::Vector2f(x, y));
-
-		rect.setFillColor(sf::Color(0, 255, 0, 255));
-		rect.setOutlineColor(sf::Color(50, 50, 50, 255));
-
-		rect.setOutlineThickness(2.0f);
-
-		window.draw(rect);
-	}
-
-	if(const Node* pCurrent = pathFinder.GetCurrentNode())
-	{
-		while(pCurrent)
+		while(pCurrentNode)
 		{
-			const float x = pCurrent->coordinates.x * width;
-			const float y = pCurrent->coordinates.y * height;
-
-			sf::RectangleShape rect(sf::Vector2f(width - 1.0f, height - 1.0f));
-			rect.setPosition(sf::Vector2f(x, y));
-
-			rect.setFillColor(sf::Color(0, 0, 255, 255));
-			rect.setOutlineColor(sf::Color(50, 50, 50, 255));
-
-			rect.setOutlineThickness(2.0f);
-
-			window.draw(rect);
-
-			pCurrent = pCurrent->pPrevious;
+			DrawNode(window, *pCurrentNode, dimensions, sf::Color::Blue);
+			pCurrentNode = pCurrentNode->pPrevious;
 		}
-		
 	}
 
+	DrawNode(window, *pathFinder.GetStartNode(), dimensions, sf::Color::Cyan);
+	DrawNode(window, *pathFinder.GetGoalNode(), dimensions, sf::Color::Cyan);
 }
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(1280, 720), "My window");
-	window.setFramerateLimit(60);
+	//window.setFramerateLimit(60);
 
 	PathFinder pathFinder(sf::Vector2u(25, 25));
-	pathFinder.Setup(sf::Vector2i(0, 0), sf::Vector2i(24, 24), 0.1f);
+	pathFinder.Setup(sf::Vector2i(0, 0), sf::Vector2i(pathFinder.m_Width - 1, pathFinder.m_Height - 1), 0.3f);
 
 	bool isSolved = false;
 
